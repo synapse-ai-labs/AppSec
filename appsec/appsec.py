49135a80c7ef18bf07cf771bb39bb86d885521e1
Aaron Joyce <joyceaa@tcd.ie>
2024-08-02 18:41:55 +0100

    Debugging

--- appsec/appsec.py
+++ appsec/appsec.py
@@ -1,19 +1,40 @@
+import os
+import json
+# import aiohttp
+
+URL = ''
+
+
+async def create_run(session, repo: str, to_event: str, from_event: str = None):
+    async with session.post(url=URL, json={
+        'repo': repo,
+        'to_event': to_event,
+        'from_event': from_event
+    }) as response:
+        return await response.json()
+
 
 async def main(
-    api_token: str,
-    commit_from: str = None,
-    commit_to: str = None
+    repo: str,
+    to_event: str,
+    from_event: str = None
 ):
-    # tasks = asyncio.create_task(
-    #
-    # )
+    api_token = os.getenv('AUTOGRAD_API_TOKEN')
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    data = {
+        "headers": headers,
+        "repo": repo,
+        "to_event": to_event,
+        "from_event": from_event
+    }
+    raise Exception(json.dumps(data))
     # try:
-    #     await asyncio.gather(*tasks)
-    # except Exception as ex:
+    #     async with aiohttp.ClientSession(headers=headers) as session:
+    #         result = await create_run(session, repo, to_event, from_event)
+    #         print(result)
+    # except aiohttp.ClientError as ex:
     #     print(str(ex))
-    try:
-        raise Exception("Raising exception")
-    except Exception as ex:
-        print(str(ex))
-        raise ex
+    #
 
