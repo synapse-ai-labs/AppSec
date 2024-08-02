c3424685db3e4ed08a91b2d5a7fcf4a79afb6ef8
Aaron Joyce <joyceaa@tcd.ie>
2024-08-02 18:48:11 +0100

    Updating

--- appsec/cli.py
+++ appsec/cli.py
@@ -7,8 +7,8 @@ from .appsec import main
 def cli():
     parser = argparse.ArgumentParser(prog="appsec", description="AppSec PR requestor")
     parser.add_argument('--repo', help='The GitHub repo to which the commit(s) belong')
-    parser.add_argument('--event_from', help='The source commit SHA or tag to compare from')
-    parser.add_argument('--event_to', help='The target commit SHA or tag to compare against')
+    parser.add_argument('--from_event', help='The source commit SHA or tag to compare from')
+    parser.add_argument('--to_event', help='The target commit SHA or tag to compare against')
 
     args = parser.parse_args()
     print(("args", args))
@@ -16,8 +16,8 @@ def cli():
     asyncio.run(
         main(
             repo=args.repo,
-            to_event=args.event_to,
-            from_event=args.event_from,
+            to_event=args.to_event,
+            from_event=args.from_event,
         )
     )
 
