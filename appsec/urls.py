6ce3ed8fe34cf5ef5d861a5f0252057425e06f1f
Aaron Joyce <joyceaa@tcd.ie>
2024-08-02 21:08:12 +0100

    Updating endpoint

--- appsec/urls.py
+++ appsec/urls.py
@@ -1,4 +1,4 @@
 API_VERSION = "v1"
-API_BASE_URL = f"https://344b-109-78-69-127.ngrok-free.app/{API_VERSION}/"
+API_BASE_URL = f"https://344b-109-78-69-127.ngrok-free.app/api/{API_VERSION}/"
 CREATE_RUN_ENDPOINT = f"{API_BASE_URL}runs/create"
 QUERY_RUN_ENDPOINT = f"{API_BASE_URL}runs/query"
