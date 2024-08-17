e32ff63feec5b5a50134e670f65d1548bebdc747
Aaron Joyce <joyceaa@tcd.ie>
2024-08-17 15:05:38 +0100

    Updating API endpoint

--- appsec/urls.py
+++ appsec/urls.py
@@ -1,4 +1,4 @@
 API_VERSION = "v1"
-API_BASE_URL = f"https://84dd-82-163-218-33.ngrok-free.app/api/{API_VERSION}/"
+API_BASE_URL = f"https://api.autograd.ai/api/{API_VERSION}/"
 CREATE_RUN_ENDPOINT = f"{API_BASE_URL}runs/create"
 QUERY_RUN_ENDPOINT = f"{API_BASE_URL}runs/query"
