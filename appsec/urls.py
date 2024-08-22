e99a9a8754f6a455776903f6fb7fc8630008eb4f
Aaron Joyce <joyceaa@tcd.ie>
2024-08-22 16:25:11 +0100

    Updating API endpoint

--- appsec/urls.py
+++ appsec/urls.py
@@ -1,4 +1,4 @@
 API_VERSION = "v1"
-API_BASE_URL = f"https://api.autograd.ai/api/{API_VERSION}/"
+API_BASE_URL = f"http://localhost/api/{API_VERSION}/"
 CREATE_RUN_ENDPOINT = f"{API_BASE_URL}runs/create"
 QUERY_RUN_ENDPOINT = f"{API_BASE_URL}runs/query"
